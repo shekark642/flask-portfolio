0170497bad5cb098ab9c793939290ab2684e58f8
shekark642 <72948731+shekark642@users.noreply.github.com>
2020-11-02 22:43:05 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+# https://flask.palletsprojects.com/en/1.1.x/api/
+import data
+from flask import Flask, render_template, request, redirect
+from data import board
+#create a Flask instance
+app = Flask(__name__)
+
+#InfoDb = []
+
+#connects default URL of server to a python function
+@app.route('/')
+def home():
+    #function uses Flask import (Jinga) to render HTML
+    #data is passed as a parameter
+    return render_template("home.html")#home has to be under templates
+
+@app.route("/chessJs")#for the dragable chess file
+def chessJS():
+
+  return render_template("chessJs.html")
+
+@app.route("/chessPush")#for the pritn chess board from dictonaries
+def chessPush():
+    return render_template("chessPush.html", board=board)
+
+@app.route("/index")#for the dragable chess file
+def index():
+    return render_template("index.html", display="")
+
+@app.route("/add", methods=['GET','POST'],)#for the dragable chess file
+def addition():
+    if request.method == 'POST':
+        form = request.form
+        numberOne = int(form['numOne'])
+        numberTwo = int(form['numTwo'])
+        calc = numberOne + numberTwo
+        return render_template("index.html", your_list = data.answersdata(calc))#data.playlist()
+
+    return redirect("/index")
+
+@app.route("/journals")#for storing all the links to the webpage
+def journals():
+  return render_template("journals.html",repl="repl of website", website ="link to personal website")#allows to define the text that is hyperlinked on the the personal journals
+
+if __name__ == "__main__":
+    #runs the application on the repl development server
+    app.run(debug=True)
